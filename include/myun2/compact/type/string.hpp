321072919d3cfbd9659ff074d44c1f492bbe166e
myun2 <myun2@nwhite.info>
2013-12-06 14:49:46 +0900

    Add Type Class of string.

---
+++ include/myun2/compact/type/string.hpp
@@ -0,0 +1,17 @@
+#ifndef __github_com_myun2__compact__type__string_HPP__
+#define __github_com_myun2__compact__type__string_HPP__
+
+namespace myun2
+{
+	namespace compact
+	{
+		namespace type
+		{
+			class string
+			{
+			};
+		}
+	}
+}
+
+#endif//__github_com_myun2__compact__type__string_HPP__
