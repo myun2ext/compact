78ee7eb5471157eb46d60ba35a5d24932e83a202
myun2 <myun2@nwhite.info>
2013-12-06 14:53:09 +0900

    base.hpp Add

---
+++ include/myun2/compact/type/base.hpp
@@ -0,0 +1,17 @@
+#ifndef __github_com_myun2__compact__type__base_HPP__
+#define __github_com_myun2__compact__type__base_HPP__
+
+namespace myun2
+{
+	namespace compact
+	{
+		namespace type
+		{
+			class base
+			{
+			};
+		}
+	}
+}
+
+#endif//__github_com_myun2__compact__type__base_HPP__
