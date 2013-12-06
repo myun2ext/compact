b2f3daaf02e847a62d624e0ecb805d6ed68150b4
myun2 <myun2@nwhite.info>
2013-12-06 14:53:36 +0900

    type/types.hpp (includes header) was add

---
+++ include/myun2/compact/type/types.hpp
@@ -0,0 +1,8 @@
+#ifndef __github_com_myun2__compact__type__types_HPP__
+#define __github_com_myun2__compact__type__types_HPP__
+
+#include "myun2/compact/type/integer.hpp"
+#include "myun2/compact/type/string.hpp"
+#include "myun2/compact/type/array.hpp"
+
+#endif//__github_com_myun2__compact__type__types_HPP__
