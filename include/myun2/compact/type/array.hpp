72cb8f351dd2c09609a2e9457e56d1ca3b32da29
myun2 <myun2@nwhite.info>
2013-12-06 17:07:21 +0900

    type of array implement.

--- include/myun2/compact/type/array.hpp
+++ include/myun2/compact/type/array.hpp
@@ -1,14 +1,24 @@
 #ifndef __github_com_myun2__compact__type__array_HPP__
 #define __github_com_myun2__compact__type__array_HPP__
 
+#include <vector>
+
 namespace myun2
 {
 	namespace compact
 	{
 		namespace type
 		{
+			template <typename T>
 			class array
 			{
+			private:
+				::std::vector<T> value;
+			public:
+				array(){}
+
+				const void* serialize_value() const { return &value.begin(); }
+				unsigned int size() const { return value.size() * sizeof(T); }
 			};
 		}
 	}
